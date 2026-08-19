8b4eef5dad27efa04d5b368406ac171192f50e4c
9M2PJU <9m2pju@gmail.com>
2026-08-20 06:52:40 +0800

    feat(cursor): switch cursor theme to Phinger Cursors Dark

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -64,9 +64,9 @@ end)
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
-hl.env("XCURSOR_THEME", "catppuccin-mocha-dark-cursors")
+hl.env("XCURSOR_THEME", "phinger-cursors-dark")
 hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_THEME", "catppuccin-mocha-dark-cursors")
+hl.env("HYPRCURSOR_THEME", "phinger-cursors-dark")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
