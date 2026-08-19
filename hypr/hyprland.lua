c9c7c823523d7c435ee850e56ce68476baeed914
9M2PJU <9m2pju@gmail.com>
2026-08-20 06:53:12 +0800

    feat(cursor): finalize default cursor to Bibata Modern Classic

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -64,9 +64,9 @@ end)
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
-hl.env("XCURSOR_THEME", "phinger-cursors-dark")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_THEME", "phinger-cursors-dark")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
