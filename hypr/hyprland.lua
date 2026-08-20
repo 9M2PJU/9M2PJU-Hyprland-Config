2bb73bf479902564742be4a1167506841c66e49d
9M2PJU <9m2pju@gmail.com>
2026-08-20 15:30:07 +0800

    feat(hypr): enable focus_on_activate for tray and app activation

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -144,6 +144,7 @@ hl.config({
         force_default_wallpaper = 0,
         disable_hyprland_logo   = true,
         disable_splash_rendering = true,
+        focus_on_activate        = true,
     },
 })
 
